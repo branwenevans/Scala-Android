2e852d171207a45d232190e5838a62d53e5e167a
Branwen Evans <evans.branwen@gmail.com>
2015-07-09 14:35:44 +1200

    changing layout

--- app/src/main/scala/com/branwenevans/scalaapp/MainActivity.scala
+++ app/src/main/scala/com/branwenevans/scalaapp/MainActivity.scala
@@ -4,12 +4,13 @@ import android.app.Activity
 import android.graphics.Bitmap
 import android.os.{AsyncTask, Bundle}
 import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
+import android.view.View
 
 import scala.collection.mutable
 
 class MainActivity extends Activity {
 
-  var dataset = mutable.MutableList[Weather]()
+  val dataset = mutable.MutableList[Weather]()
 
   val adapter = new ListAdapter(dataset)
 
@@ -20,18 +21,23 @@ class MainActivity extends Activity {
     val layoutManager = new LinearLayoutManager(getBaseContext)
     val view = findViewById(R.id.recycler_view).asInstanceOf[RecyclerView]
 
+    view.setElevation(5)
     view.setAdapter(adapter)
     view.setHasFixedSize(true)
     view.setLayoutManager(layoutManager)
 
-
-    new DownloadWeatherDataTask().execute("Christchurch,NZ")
+    refreshWeather(view);
   }
 
-  class DownloadWeatherDataTask extends AsyncTask[AnyRef, AnyRef, AnyRef] {
+  def refreshWeather(view: View) {
+    new RefreshWeatherDataTask().execute("Christchurch,NZ")
+  }
+
+  class RefreshWeatherDataTask extends AsyncTask[AnyRef, AnyRef, AnyRef] {
 
     //params are AnyRef to get around Scala's overriding vararg method issue: http://issues.scala-lang.org/browse/SI-1459
     override def doInBackground(params: AnyRef*): AnyRef = {
+      dataset.clear
       dataset ++= WeatherLoader.getWeather(params.head.asInstanceOf[String])
     }
 
