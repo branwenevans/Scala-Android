439a5ce8ccaca8abb3939bbde86a59f69ea2d37f
Branwen Evans <evans.branwen@gmail.com>
2015-07-10 15:12:14 +1200

    using device location, added default location since emulator has issues with getting last location, added refresh location action

--- app/src/main/scala/com/branwenevans/scalaapp/MainActivity.scala
+++ app/src/main/scala/com/branwenevans/scalaapp/MainActivity.scala
@@ -1,21 +1,36 @@
 package com.branwenevans.scalaapp
 
 import android.app.Activity
+import android.content.Context
+import android.location.{LocationManager, Location}
 import android.os.{AsyncTask, Bundle}
 import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
-import android.view.View
+import android.view.{Menu, MenuInflater, MenuItem}
+import com.google.android.gms.common.{GooglePlayServicesUtil, ConnectionResult}
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.GoogleApiClient.{Builder, ConnectionCallbacks, OnConnectionFailedListener}
+import com.google.android.gms.location.LocationServices
 
 import scala.collection.mutable
 
-class MainActivity extends Activity {
+class MainActivity extends Activity with ConnectionCallbacks with OnConnectionFailedListener {
 
   val dataset = mutable.MutableList[Weather]()
 
   val adapter = new WeatherListAdapter(dataset)
 
+  var googleApiClient = None: Option[GoogleApiClient]
+
+  var location = None: Option[Location]
+
+  val defaultLocation: Location = new Location("default")
+
   protected override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    defaultLocation.setLatitude(-43.5358331);
+    defaultLocation.setLongitude(172.6054242);
 
     val layoutManager = new LinearLayoutManager(getBaseContext)
     val view = findViewById(R.id.recycler_view).asInstanceOf[RecyclerView]
@@ -25,19 +40,51 @@ class MainActivity extends Activity {
     view.setHasFixedSize(true)
     view.setLayoutManager(layoutManager)
 
-    refreshWeather(view);
+    val result: Int = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+    if (GooglePlayServicesUtil.isUserRecoverableError(result)) {
+      GooglePlayServicesUtil.showErrorDialogFragment(result, this, 1)
+    }
+
+    googleApiClient = Option(new Builder(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build())
   }
 
-  def refreshWeather(view: View) {
-    new RefreshWeatherDataTask().execute("Christchurch,NZ")
+
+  override def onStart() {
+    super.onStart()
+    googleApiClient.get.connect()
+    refreshWeather()
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    val inflater: MenuInflater = getMenuInflater
+    inflater.inflate(R.menu.menu_main, menu)
+    super.onCreateOptionsMenu(menu)
+  }
+
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    super.onOptionsItemSelected(item)
+    item.getItemId match {
+      case R.id.action_location => updateLocation()
+    }
+  }
+
+  def updateLocation(): Boolean = {
+    location = Option(LocationServices.FusedLocationApi.getLastLocation(googleApiClient.get))
+    refreshWeather()
+    true
+  }
+
+  def refreshWeather() {
+    new RefreshWeatherDataTask().execute(location.getOrElse(defaultLocation))
   }
 
   class RefreshWeatherDataTask extends AsyncTask[AnyRef, AnyRef, AnyRef] {
 
     //params are AnyRef to get around Scala's overriding vararg method issue: http://issues.scala-lang.org/browse/SI-1459
     override def doInBackground(params: AnyRef*): AnyRef = {
-      dataset.clear
-      dataset ++= WeatherLoader.getWeather(params.head.asInstanceOf[String])
+      dataset.clear()
+      dataset ++= WeatherLoader.getWeather(params.head.asInstanceOf[Location])
     }
 
     override def onPostExecute(result: AnyRef) {
@@ -45,6 +92,17 @@ class MainActivity extends Activity {
     }
   }
 
+  override def onConnectionSuspended(i: Int) {
+    print("connection suspended")
+  }
+
+  override def onConnected(bundle: Bundle) {
+    location = Option(LocationServices.FusedLocationApi.getLastLocation(googleApiClient.get))
+  }
+
+  override def onConnectionFailed(connectionResult: ConnectionResult) {
+    print("connection failed")
+  }
 }
 
 
--- app/src/main/scala/com/branwenevans/scalaapp/WeatherLoader.scala
+++ app/src/main/scala/com/branwenevans/scalaapp/WeatherLoader.scala
@@ -3,10 +3,11 @@ package com.branwenevans.scalaapp
 import java.io.InputStream
 import java.net.{HttpURLConnection, URL}
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import java.util.{TimeZone, Date}
 
+import android.location.Location
 import android.text.format.DateUtils
-import org.json.{JSONArray, JSONObject}
+import org.json.JSONObject
 
 import scala.collection.mutable
 import scala.io.Source
@@ -14,17 +15,18 @@ import scala.io.Source
 object WeatherLoader {
 
   val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="
-
+  val LAT = "lat="
+  val LNG = "&lon="
   val UNITS = "&units=metric"
-
   val DAYS = "&cnt=7"
 
-  def getWeather(location: String): mutable.MutableList[Weather] = {
+
+  def getWeather(location: Location): mutable.MutableList[Weather] = {
     parseWeatherJson(getWeatherJson(location))
   }
 
-  def getWeatherJson(location: String): String = {
-    val connection = new URL(URL + location + UNITS + DAYS).openConnection().asInstanceOf[HttpURLConnection]
+  def getWeatherJson(location: Location): String = {
+    val connection = new URL(URL + LAT + location.getLatitude + LNG + location.getLongitude + UNITS + DAYS).openConnection().asInstanceOf[HttpURLConnection]
     var stream = None: Option[InputStream]
     try {
       connection.connect()
@@ -32,8 +34,8 @@ object WeatherLoader {
       Source.fromInputStream(stream.get).mkString
     }
     finally {
-      stream.foreach(s => s.close)
-      connection.disconnect
+      stream.foreach(s => s.close())
+      connection.disconnect()
     }
   }
 
@@ -55,5 +57,7 @@ object WeatherLoader {
 }
 
 class Weather(private val timestamp: Long, val heading: String, val description: String, val code: Int, val temp: Long, val minTemp: Long, val maxTemp: Long) {
-  val date = if(DateUtils.isToday(timestamp * 1000)) "Today" else new SimpleDateFormat("EEEE yyyy-MM-dd").format(new Date(timestamp * 1000))
+  val formatter = new SimpleDateFormat("EEEE yyyy-MM-dd")
+  formatter.setTimeZone(TimeZone.getDefault)
+  val date = if (DateUtils.isToday(timestamp * 1000)) "Today" else formatter.format(new Date(timestamp * 1000))
 }
